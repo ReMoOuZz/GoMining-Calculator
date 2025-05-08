132ab0d166e124ab80930b5094b26c94c8c5baee
ReMoOuZz <203637481+ReMoOuZz@users.noreply.github.com>
2025-05-08 14:26:37 +0200

    Update
    Feat: add BTC and GMT price display (HTML, CSS, JS)
    
    Feat : Add a connexion button and a language  selector (HMTL, CSS)
    
    style: enhance header styling

---
+++ main.js
@@ -0,0 +1,15 @@
+// async function getCryptoPrices() {
+//     const url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,gmt-token&vs_currencies=usd';
+
+//     try {
+//         const response = await fetch(url)
+//         const data = await response.json()
+
+//         document.getElementById("btc-price").textContent = "$ " + data.bitcoin.usd
+//         document.getElementById("gmt-price").textContent = "$ " + data["gmt-token"].usd
+//     } catch (error) {
+//         console.error("erreur lors de la récupération des prix :", error)
+//     }
+// }
+
+// getCryptoPrices()
