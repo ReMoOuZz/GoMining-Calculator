fa63dd9483812f23998204af91b1d73ce774b599
ReMoOuZz <203637481+ReMoOuZz@users.noreply.github.com>
2025-05-08 18:17:42 +0200

    Update
    fix: Rename <h6> classes and apply crypto color styles
    
    feat: Add Bitcoin and GoMining color themes to inputs input .

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-async function getCryptoPrices() {
-    const url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,gmt-token&vs_currencies=usd';
+// async function getCryptoPrices() {
+//     const url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,gmt-token&vs_currencies=usd';
 
-    try {
-        const response = await fetch(url)
-        const data = await response.json()
+//     try {
+//         const response = await fetch(url)
+//         const data = await response.json()
 
-        document.getElementById("btc-price").textContent = "$ " + data.bitcoin.usd
-        document.getElementById("gmt-price").textContent = "$ " + data["gmt-token"].usd
-    } catch (error) {
-        console.error("erreur lors de la récupération des prix :", error)
-    }
-}
+//         document.getElementById("btc-price").textContent = "$ " + data.bitcoin.usd
+//         document.getElementById("gmt-price").textContent = "$ " + data["gmt-token"].usd
+//     } catch (error) {
+//         console.error("erreur lors de la récupération des prix :", error)
+//     }
+// }
 
-getCryptoPrices()
+// getCryptoPrices()
 
 // const toggle = document.getElementById('mode-toggle')
 // toggle.addEventListener('change', ()=> {
