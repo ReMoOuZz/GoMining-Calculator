d9f15ba8bd4c8f5938711a79492805f7d92e851d
ReMoOuZz <203637481+ReMoOuZz@users.noreply.github.com>
2025-05-08 17:45:36 +0200

    Update
    feat : Start to build the calculator, HMTL, CSS and JS
    
    feat: Add placeholder variable for future BTC/GMT content
    
    style : Add style to calculator layout

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-// async function getCryptoPrices() {
-//     const url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,gmt-token&vs_currencies=usd';
+async function getCryptoPrices() {
+    const url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,gmt-token&vs_currencies=usd';
 
-//     try {
-//         const response = await fetch(url)
-//         const data = await response.json()
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
 
-//         document.getElementById("btc-price").textContent = "$ " + data.bitcoin.usd
-//         document.getElementById("gmt-price").textContent = "$ " + data["gmt-token"].usd
-//     } catch (error) {
-//         console.error("erreur lors de la récupération des prix :", error)
+        document.getElementById("btc-price").textContent = "$ " + data.bitcoin.usd
+        document.getElementById("gmt-price").textContent = "$ " + data["gmt-token"].usd
+    } catch (error) {
+        console.error("erreur lors de la récupération des prix :", error)
+    }
+}
+
+getCryptoPrices()
+
+// const toggle = document.getElementById('mode-toggle')
+// toggle.addEventListener('change', ()=> {
+//     if(toggle.checked) {
+//         console.log("mode BTC");
+//     } else  {
+//         console.log("Mode GMT");
 //     }
-// }
-
-// getCryptoPrices()
+// })
