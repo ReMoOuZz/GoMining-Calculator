39fa8a0cdec10f5898ef7da80d509bbbdc31b7bc
remi <203637481+ReMoOuZz@users.noreply.github.com>
2025-12-31 12:39:29 +0100

    adjusted satoshis per TH

--- calculator.js
+++ calculator.js
@@ -7,11 +7,20 @@ const baseValues = {
 };
 
 function updateBaseValues() {
-  baseValues.income = parseFloat(document.getElementById("income").textContent) || 0;
-  baseValues.discount = parseFloat(document.getElementById("discount-percentage-btc").textContent) || 0;
-  baseValues.discountGmt = parseFloat(document.getElementById("discount-percentage-gmt").textContent) || 0;
-  baseValues.profit = parseFloat(document.getElementById("profit").textContent) || 0;
-  baseValues.profitDollar = parseFloat(document.getElementById("profit-dollar").textContent) || 0;
+  baseValues.income =
+    parseFloat(document.getElementById("income").textContent) || 0;
+  baseValues.discount =
+    parseFloat(
+      document.getElementById("discount-percentage-btc").textContent
+    ) || 0;
+  baseValues.discountGmt =
+    parseFloat(
+      document.getElementById("discount-percentage-gmt").textContent
+    ) || 0;
+  baseValues.profit =
+    parseFloat(document.getElementById("profit").textContent) || 0;
+  baseValues.profitDollar =
+    parseFloat(document.getElementById("profit-dollar").textContent) || 0;
 }
 
 function recalculateRewards() {
@@ -20,7 +29,7 @@ function recalculateRewards() {
   const thPower = parseFloat(document.getElementById("th-power").value);
   const efficiency = parseFloat(document.getElementById("efficiency").value);
   const discount = parseFloat(document.getElementById("discount").value);
-  const tauxBtc = 44;
+  const tauxBtc = 43;
 
   if (
     isNaN(btcPrice) ||
@@ -49,8 +58,10 @@ function recalculateRewards() {
   const service = (0.0089 / btcPrice) * thPower;
   const serviceGmt = (0.0089 / gmtPrice) * thPower;
 
-  const electricityConsomationFinal = electricityConsomationBtc * (1 - discount / 100);
-  const electricityConsomationFinalGmt = electricityConsomationGmt * (1 - discount / 100);
+  const electricityConsomationFinal =
+    electricityConsomationBtc * (1 - discount / 100);
+  const electricityConsomationFinalGmt =
+    electricityConsomationGmt * (1 - discount / 100);
   const serviceFinal = service * (1 - discount / 100);
   const serviceFinalGmt = serviceGmt * (1 - discount / 100);
 
@@ -68,16 +79,36 @@ function recalculateRewards() {
   if (mode === "monthly") multiply = 30;
   else if (mode === "yearly") multiply = 365;
 
-  document.getElementById("income").textContent = (btcBrutRewards * multiply).toFixed(8);
-  document.getElementById("discount-percentage-btc").textContent = ((electricityConsomationFinal + serviceFinal) * multiply).toFixed(8);
-  document.getElementById("discount-percentage-gmt").textContent = ((electricityConsomationFinalGmt + serviceFinalGmt) * multiply).toFixed(2);
-  document.getElementById("profit").textContent = (totalReward * multiply).toFixed(8);
-  document.getElementById("profit-dollar").textContent = (totalRewardDollar * multiply).toFixed(2);
+  document.getElementById("income").textContent = (
+    btcBrutRewards * multiply
+  ).toFixed(8);
+  document.getElementById("discount-percentage-btc").textContent = (
+    (electricityConsomationFinal + serviceFinal) *
+    multiply
+  ).toFixed(8);
+  document.getElementById("discount-percentage-gmt").textContent = (
+    (electricityConsomationFinalGmt + serviceFinalGmt) *
+    multiply
+  ).toFixed(2);
+  document.getElementById("profit").textContent = (
+    totalReward * multiply
+  ).toFixed(8);
+  document.getElementById("profit-dollar").textContent = (
+    totalRewardDollar * multiply
+  ).toFixed(2);
 
   if (isGmtMode) {
-    document.getElementById("daily-charging-cost").textContent = (electricityConsomationFinalGmt + serviceFinalGmt).toFixed(2);
-    document.getElementById("monthly").textContent = ((electricityConsomationFinalGmt + serviceFinalGmt) * 30).toFixed(2);
-    document.getElementById("yearly").textContent = ((electricityConsomationFinalGmt + serviceFinalGmt) * 365).toFixed(2);
+    document.getElementById("daily-charging-cost").textContent = (
+      electricityConsomationFinalGmt + serviceFinalGmt
+    ).toFixed(2);
+    document.getElementById("monthly").textContent = (
+      (electricityConsomationFinalGmt + serviceFinalGmt) *
+      30
+    ).toFixed(2);
+    document.getElementById("yearly").textContent = (
+      (electricityConsomationFinalGmt + serviceFinalGmt) *
+      365
+    ).toFixed(2);
   }
 
   updateBaseValues();
@@ -87,4 +118,6 @@ function recalculateRewards() {
 //   input.addEventListener("change", recalculateRewards);
 // });
 
-document.getElementById("period").addEventListener("change", recalculateRewards);
+document
+  .getElementById("period")
+  .addEventListener("change", recalculateRewards);
